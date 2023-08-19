2053cdf7ec6b1fa0aba0e7fec541503e7d773f7b
abbas <dashatanad@gmail.com>
2023-08-19 19:13:58 +0330

    auth

--- app/api/auth/[...nextAuth]/options.ts
+++ app/api/auth/[...nextAuth]/options.ts
@@ -1,5 +1,33 @@
 import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        const res = await fetch("", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        })
+        const user = await res.json()
+        if (res.ok && user) {
+          return user
+        }
+        return null
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  }
 }
